55c9819b5fab3d287cf8e228695ee8036c924eb5
Santosh Kumar Jami <jami.sk403@gmail.com>
2021-10-26 15:34:45 +0530

    Config and yolo Model
    Config File: Architecture
    Yolo Model : Yolo Reshape and Yolo Model Development

--- tensorflow-keras/object-detection/YOLOV1/config.py
+++ tensorflow-keras/object-detection/YOLOV1/config.py
@@ -7,4 +7,35 @@ config['classes'] = ['aeroplane', 'bicycle', 'boat', 'bus', 'car', 'motorbike', 
 config['class_map'] = {k: idx for idx, k in enumerate(config['classes'])}
 config['n_classes'] = len(config['classes'])
 config['output_shape'] = (config['grid_size'][0], config['grid_size'][1], config['n_boxes'] * 5 + config['n_classes'])
+config['model_arch'] = [  # tuple : (block, kernel_size, filters, stride, padding)
+    # tuple : (block, pool_size, stride_size, padding)
+    ('CNN', 7, 64, 2, 'same'),
+    ('Max', 2, 2, 'same'),
+    ('CNN', 3, 192, 1, 'same'),
+    ('Max', 2, 2, 'same'),
+    ('CNN', 1, 128, 1, 'same'),
+    ('CNN', 3, 256, 1, 'same'),
+    ('CNN', 1, 256, 1, 'same'),
+    ('CNN', 3, 512, 1, 'same'),
+    ('Max', 2, 2, 'same'),
+    ('CNN', 1, 256, 1, 'same'),
+    ('CNN', 3, 512, 1, 'same'),
+    ('CNN', 1, 256, 1, 'same'),
+    ('CNN', 3, 512, 1, 'same'),
+    ('CNN', 1, 256, 1, 'same'),
+    ('CNN', 3, 512, 1, 'same'),
+    ('CNN', 1, 256, 1, 'same'),
+    ('CNN', 3, 512, 1, 'same'),
+    ('CNN', 1, 512, 1, 'same'),
+    ('CNN', 3, 1024, 1, 'same'),
+    ('Max', 2, 2, 'same'),
+    ('CNN', 1, 512, 1, 'same'),
+    ('CNN', 3, 1024, 1, 'same'),
+    ('CNN', 1, 512, 1, 'same'),
+    ('CNN', 3, 1024, 1, 'same'),
+    ('CNN', 3, 1024, 1, 'same'),
+    ('CNN', 3, 1024, 2, 'same'),
+    ('CNN', 3, 1024, 1, 'same'),
+    ('CNN', 3, 1024, 1, 'same')
+]
 
--- tensorflow-keras/object-detection/YOLOV1/yolo_model.py
+++ tensorflow-keras/object-detection/YOLOV1/yolo_model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, MaxPooling2D
+from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, MaxPooling2D, Input, Dense, Flatten, Dropout
 from tensorflow.keras.regularizers import l2
+import tensorflow.keras.backend as K
 from glob import glob
 from tqdm import tqdm
 import numpy as np
@@ -16,16 +17,75 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 print(f"Tensorflow Version: {tf.__version__}")
 
 class yolov1():
-    def __init__(self):
+    def __init__(self, config):
         super(yolov1, self).__init__()
+        self.config = config
+        self.architecture = config['model_arch']
 
-    def CNNBlock(self, x, nfilters, size, stride, pool=None):
-        x = Conv2D(filters=nfilters, kernel_size=size, padding='same', strides=stride, kernel_initializer='he_normal',
-                   kernel_regularizer=l2(5e-4))(x)
+    def CNNBlock(self, x, kernel_size, nfilters, stride, padding):
+        x = Conv2D(filters=nfilters, kernel_size=kernel_size, padding=padding, strides=stride,
+                   kernel_initializer='he_normal', kernel_regularizer=l2(5e-4))(x)
         x = BatchNormalization()(x)
         x = LeakyReLU(alpha=0.1)(x)
-        if pool is not None:
-            x = MaxPooling2D(pool_size=pool)(x)
         return x
+
+    def create_darknet(self, input_shape):
+        input_layer = Input(shape=(*input_shape, 3))
+        x = input_layer
+        for block in self.architecture:
+            if block[0] == 'CNN':
+                x = self.CNNBlock(x, *block[1:])
+            elif block[0] == 'Max':
+                x = MaxPooling2D(pool_size=block[1], strides=block[2], padding=block[3])(x)
+        x = Flatten()(x)
+        return x
+
+    def add_fc_layers(self, x, output_shape):
+        x = Dense(512, linear=False)(x)
+        x = Dense(5096, linear=False)(x)
+        x = Dropout(0.5)(x)
+        x = Dense(1470, linear=True)(x)
+        return x
+
+
+class yolo_reshape(tf.keras.layers):
+    def __init__(self, config):
+        super(yolo_reshape, self).__init__()
+        self.target_shape = config['output_shape']
+        self.config = config
+
+    def __call__(self, input):
+        # get_grid size
+        S = [self.target_shape[0], self.target_shape[1]]
+        # classes
+        C = self.config['n_classes']
+        # boxes
+        B = self.config['n_boxes']
+
+        idx1 = S[0] * S[1] * C #each block first C values
+        idx2 = idx1 + S[0]*S[1]*B
+
+        # class probabilities
+        class_prob = K.reshape(input[:, :idx1], (K.shape(input)[0], ) + tuple(S[0], S[1], C))
+        class_prob = K.softmax(class_prob)
+
+        # confidence
+        confs = K.reshape(input[:, idx1:idx2], (K.shape(input)[0], ) + tuple(S[0], S[1], B))
+        confs = K.sigmoid(confs)
+
+        # Boxes
+        boxes = K.reshape(input[:, idx2:], (K.shape(input)[0], ) + tuple(S[0], S[1], B*4))
+
+        outputs = K.concatenate([class_prob, confs, boxes])
+
+        return outputs
+
+
+    
+
+
+
+
+
 
 
